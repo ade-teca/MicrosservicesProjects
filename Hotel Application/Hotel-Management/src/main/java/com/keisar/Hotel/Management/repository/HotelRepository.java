6fb84109b255501b30fd556edfec4f38b7ed026a
ade-teca <adelitoteca@gmail.com>
2026-02-04 09:09:42 -0300

    feat(service): implement business logic for hotel management
    Criação do Service com métodos de salvamento e conversão DTO/Entity.

--- Hotel Application/Hotel-Management/src/main/java/com/keisar/Hotel/Management/repository/HotelRepository.java
+++ Hotel Application/Hotel-Management/src/main/java/com/keisar/Hotel/Management/repository/HotelRepository.java
@@ -2,6 +2,8 @@ package com.keisar.Hotel.Management.repository;
 
 import com.keisar.Hotel.Management.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 }
---
+++ Hotel Application/Hotel-Management/src/main/java/com/keisar/Hotel/Management/service/HotelService.java
@@ -0,0 +1,51 @@
+package com.keisar.Hotel.Management.service;
+
+import com.keisar.Hotel.Management.dto.response.HotelResponseDTO;
+import com.keisar.Hotel.Management.dto.response.request.HotelRequestDTO;
+import com.keisar.Hotel.Management.dto.response.request.HotelUpdateAddressDTO;
+import com.keisar.Hotel.Management.model.Hotel;
+import com.keisar.Hotel.Management.repository.HotelRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class HotelService {
+
+    private final HotelRepository hotelRepository;
+    private final ModelMapper modelMapper;
+
+    public HotelResponseDTO createHotel(HotelRequestDTO hotelRequestDTO) {
+        Hotel hotel = modelMapper.map(hotelRequestDTO, Hotel.class);
+        return modelMapper.map(hotelRepository.save(hotel), HotelResponseDTO.class);
+    }
+
+    public List<HotelResponseDTO> getAllHotels() {
+        return hotelRepository.findAll().stream()
+                .map(hotel-> modelMapper.map(hotel, HotelResponseDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public HotelResponseDTO updateHotel(Long id, HotelUpdateAddressDTO updateAddressDTO) {
+        Hotel hotel = hotelRepository.findById(id).orElseThrow(()-> new RuntimeException("Hotel not found"));
+        modelMapper.map(updateAddressDTO, hotel);
+        return modelMapper.map(hotelRepository.save(hotel), HotelResponseDTO.class);
+    }
+
+    public void deleteHotel(Long id) {
+        if (hotelRepository.existsById(id)) {
+            throw new RuntimeException("Hotel not found");
+        }
+        hotelRepository.deleteById(id);
+    }
+
+
+
+
+
+
+}
